8bd210c210d9c802ca4710958202e04e3784faf1
Vũ Trọng Hiếu <85973353+piuconal@users.noreply.github.com>
2025-06-18 07:00:24 +0700

    up

--- js/script.js
+++ js/script.js
@@ -14,6 +14,59 @@ function closePopup() {
   audio.currentTime = 0;
 }
 
+const countrySlider = document.getElementById("countrySlider");
+const countryLabel = document.getElementById("countryLabel");
+
+const countries = [
+  "Vietnam",
+  "USA",
+  "Canada",
+  "UK",
+  "Australia",
+  "North Pole",
+  "South Pole",
+  "Atlantis",
+  "Narnia",
+  "Zebes",
+  "Middle Earth",
+  "Wakanda",
+  "Mars Colony",
+  "Jupiter Station",
+  "Nowhere",
+  "Somewhere Else",
+  "Area 51",
+  "Under the Bed",
+  "Your Imagination",
+  "Mushroom Kingdom",
+  "Hyrule",
+  "Cyberdyne",
+  "Gotham",
+  "Metropolis",
+  "Springfield",
+  "The Upside Down",
+  "Ocean Floor",
+  "Cloud City",
+  "Tatooine",
+  "Dagobah",
+  "Asgard",
+  "Helheim",
+  "Valhalla",
+  "Pandora",
+  "District 12",
+  "Neo Tokyo",
+  "The Matrix",
+  "Raccoon City",
+  "Silent Hill",
+  "Bikini Bottom",
+  "The Shire",
+  "The Void",
+];
+
+countrySlider.addEventListener("input", function () {
+  const index = parseInt(this.value);
+  countryLabel.textContent = countries[index];
+});
+
 document.getElementById("name").addEventListener("input", function () {
   alert("Are you sure this is your real name?");
 });
